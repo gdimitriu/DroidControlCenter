8b72bbaba705ecc27edc7ceb950dc16b78c69548
gdimitriu <gabriel.dimitriu@yahoo.com>
2021-12-14 12:03:57 -0800

    first skeleton version

---
+++ app/src/main/java/io/github/gdimitriu/droidcontrolcenter/MainActivity.kt
@@ -0,0 +1,46 @@
+package io.github.gdimitriu.droidcontrolcenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.lifecycle.ViewModelProvider
+private const val TAG = "DroidControlMain"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = DroidControlFragment.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+    }
+
+    /* create the menu */
+    override fun onCreateOptionsMenu(menu: Menu) :Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.activity_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d(TAG, "DroidConnectionFragment")
+        return when (item.itemId) {
+            R.id.droid_connection -> {
+                val fragment =
+                    DroidConnectionFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null).commit()
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+}
